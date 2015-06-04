67337f0192810335e20780e3345ca3fd8725f2f9
rboyd-dt <richard.boyd@dynatrace.com>
2015-06-04 08:08:49 -0400

    initial import of Generic Execution Plugin
    wrapper created and GE plugin imported

--- src/com/dynatrace/diagnostics/plugin/NetCoolEventAction.java
+++ src/com/dynatrace/diagnostics/plugin/NetCoolEventAction.java
@@ -9,109 +9,27 @@ package com.dynatrace.diagnostics.plugin;
 
 import com.dynatrace.diagnostics.pdk.*;
 
-import java.io.*;
 import java.security.InvalidParameterException;
+import java.util.Collection;
 import java.util.logging.Logger;
-import com.tivoli.tec.event_delivery.*;
+
+import com.dynatrace.diagnostics.plugin.extendedexecutor.*;
+
+public class NetCoolEventAction extends GenericExecutor implements Action {
 
 
-public class NetCoolEventAction implements Action {
-
-	private static final Logger log = Logger.getLogger(NetCoolEventAction.class.getName());
-	private static final String SERVER_NAME = "server";
-	private static final String SERVER_PORT = "port";
-	
-	private String serverName;
-	private String serverPort;
-	
-	TECAgent sender;
-	
-	/**
-	 * Initializes the Action Plugin. This method is always 
-	 * called before <tt>execute</tt>.
-	 * If the returned status is <tt>null</tt> or the status code is a
-	 * non-success code then <tt>execute</tt> will not be called.
-	 *
-	 * @param env the configured <tt>ActionEnvironment</tt> for this Plugin;
-	 *            <b>does not contain any incidents</b>
-	 * @see Plugin#teardown()
-	 * @return a <tt>Status</tt> object that describes the result of the
-	 *         method call
-	 * @throws Exception
-	 */
 	@Override
 	public Status setup(ActionEnvironment env) throws Exception {
-
-		String cfg;
-		Reader configStream;
-		
-		try {
-			serverName = env.getConfigString(SERVER_NAME);
-			serverPort = env.getConfigString(SERVER_PORT);
-		} catch (NullPointerException ex) {
-			return new Status(Status.StatusCode.ErrorInternal, "Missing configuration property", "Missing configuration property", ex);
-		} catch (InvalidParameterException ipe) {
-			return new Status(Status.StatusCode.ErrorInternal, "Invalid configuration property", "Invalid configuration property", ipe);
-		}
-		
-		cfg = "ServerLocation=" + serverName + 
-				"\nServerPort=" + serverPort +
-				"\nBufferEvents=MEMORY_ONLY" +
-				"\nBufferFlushRate=0";
-		log.info(cfg);
-		
-		configStream = new StringReader(cfg);
-		try {
-		sender = new TECAgent(configStream, TECAgent.SENDER_MODE, true);
-		} catch (EDException edex) {
-			return new Status(Status.StatusCode.ErrorTargetService, "Error initializing sending agent", "Error initializing sending agent", edex);
-		}
-		return new Status(Status.StatusCode.Success);
+		return super.setup(env);
 	}
 
-	/**
-	 * Executes the Action Plugin to process incidents.
-	 *
-	 * <p>
-	 * This method is called at the scheduled intervals, but only if incidents
-	 * occurred in the meantime. If the Plugin execution takes longer than the
-	 * schedule interval, subsequent calls to
-	 * {@link #execute(ActionEnvironment)} will be skipped until this method
-	 * returns. After the execution duration exceeds the schedule timeout,
-	 * {@link TaskEnvironment#isStopped()} will return <tt>true</tt>. In this
-	 * case execution should be stopped as soon as possible. If the Plugin
-	 * ignores {@link TaskEnvironment#isStopped()} or fails to stop execution in
-	 * a reasonable timeframe, the execution thread will be stopped ungracefully
-	 * which might lead to resource leaks!
-	 *
-	 * @param env
-	 *            a <tt>ActionEnvironment</tt> object that contains the Plugin
-	 *            configuration and incidents
-	 * @return a <tt>Status</tt> object that describes the result of the
-	 *         method call
-	 */	@Override
+	@Override
 	public Status execute(ActionEnvironment env) throws Exception {
-		/*
-		// this sample shows how to receive and act on incidents
-		Collection<Incident> incidents = env.getIncidents();
-		for (Incident incident : incidents) {
-			String message = incident.getMessage();
-			log.info("Incident " + message + " triggered.");
-			for (Violation violation : incident.getViolations()) {
-				log.info("Measure " + violation.getViolatedMeasure().getName() + " violoated threshold.");
-			}
-		}
-		*/
-		return new Status(Status.StatusCode.Success);
+		return super.execute(env);
 	}
-	/**
-	 * Shuts the Plugin down and frees resources. This method is called either way
-	 * if the Action setup/execution has failed or was successful.
-	 *
-	 * @see Action#setup(ActionEnvironment)
-	 */
+
 	@Override
 	public void teardown(ActionEnvironment env) throws Exception {
-		// TODO
+		super.teardown(env);
 	}
 }
